6c9c2c965dc8fe71bfe19f5ec9b2a26847c2d381
woeshiga <egor.grebenckin16@gmail.com>
2024-12-01 01:34:32 +0300

    Vec summ programm

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+trait Sum {
+    fn summa(&self) -> String;
+}
+
+struct Vector {
+    numbers: Vec<i32>
+}
+
+impl Sum for Vector {
+    fn summa(&self) -> String {
+        format!("Сумма: {}", self.numbers.iter().sum::<i32>())
+    }
+}
+
+impl Vector {
+    fn new(numbers: Vec<i32>) -> Self {
+        Self { numbers }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let vector = Vector::new(vec![1, 2, 3, 4, 5]);
+    println!("Сумма: {}", vector.summa());
 }
